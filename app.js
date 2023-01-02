89976721edad6af809e93a3444e67e555f69f18f
pequito193 <vitorspequito@gmail.com>
2023-01-02 13:50:44 +0000

    Update users route

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', userRouter);
-app.use('/post', postRouter);
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
