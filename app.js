b2d1851989b2d2b462cd57f47ea8a025b13a5392
pequito193 <vitorspequito@gmail.com>
2023-01-04 10:48:38 +0000

    Fix bug

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,6 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
+
+app.get('/', (req, res) => {
+  res.redirect('/posts');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,87 @@
 var express = require('express');
 var router = express.Router();
 const User = require('./../models/user_model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-router.get('/', (req, res) => {
-    res.json('get request received')
-})
+router.post('/signup', (req, res) => {
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
 
-router.post('/sign-up', (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
-    });
-    user.save((err) => {
-        if (err) {
-            res.json({error: err});
+  // Check if password fields match
+  if (!(password === confirmPassword)) {
+    res.json({message: 'Passwords do not match!'});
+    return;
+  };
+
+  // Check if username already exists
+  User.countDocuments({username: req.body.username}, function(err, count) {
+    console.log(req.body.username, count)
+    if (count > 0) {
+      res.json({message: 'Username already taken!'});
+      return;
+    }
+
+    else {
+      // Encrypt password and create the user
+      bcrypt.hash(`${password}`, 10, (err, hashedPassword) => {
+        if(err) {
+          return next(err);
         }
-    })
+
+        else {
+            const user = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: hashedPassword
+        })
+        .save(err => {
+            if (err) { 
+                return next(err);
+            }
+        })
+        
+        res.end;
+
+      };
+      });
+    }
+  });
+
 })
 
-router.post('/log-in', (req, res) => {
+router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const requestPassword = req.body.password;
     
+    // Check username
+    User.findOne({username: username}, function(err, user) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        res.json({message: 'User not found!'});
+        return;
+      }
+
+      bcrypt.compare(requestPassword, user.password, (err, result) => {
+        if (err) {
+          return next(err);
+        }
+        if (!result) {
+          res.json({message: 'Incorrect password!'});
+          return;
+        }
+        if (result) {
+          console.log('yay!')
+          const user = {name: username};
+          const accessToken = jwt.sign(user, process.env.SECRET_ACCESS_KEY);
+          res.json({accessToken: accessToken});
+
+        }
+      })
+    })
 })
 
 module.exports = router;
