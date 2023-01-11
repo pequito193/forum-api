f61e78cafa03dff0d744d84bbd570082331e9aba
pequito193 <vitorspequito@gmail.com>
2023-01-11 13:56:02 +0000

    Update user model

--- models/user_model.js
+++ models/user_model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     username: {type: String, required: true},
+    username_lowercase: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true}
 });
