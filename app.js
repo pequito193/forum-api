334267211d8d0cf4ebed6ae51ea4ab61138616de
pequito193 <vitorspequito@gmail.com>
2023-01-30 16:59:43 +0000

    Add cors library

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var userRouter = require('./routes/user');
 var postRouter = require('./routes/posts');
@@ -27,6 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user', userRouter);
